e302e1680a6db9200a9116107af0ef2893c10092
Ngawangsonamyoezer <160131332+Ngawangsonamyoezer@users.noreply.github.com>
2024-02-29 12:04:24 +0600

    Add files via upload

---
+++ class activity.py
@@ -0,0 +1,9 @@
+name = input("enter you name:")
+num1 = float(input("enter a your first number:"))
+num2 = float(input("enter your second number:"))
+
+addition_result = num1 + num2
+subtraction_result = num1 - num2
+multiplication_result = num1 * num2
+division_result = num1 / num2
+print(f"hellow")
