e52ddfdffa60d30c0a830e2d4590a306e74250a7
ngawang yoezer <02230066.cst@rub.edu.bt>
2024-05-22 14:31:27 +0600

    fg

--- guessnumbergame.py
+++ guessnumbergame.py
@@ -1,2 +1,24 @@
-import random
-secret_number = 
+def find_first_repeating_character(s):
+   
+    char_count = {}
+    
+    for char in s:
+        if char in char_count:
+           
+            print(f"First repeating character: {char}")
+            print(f"Memory address: {id(char)}")
+            return char, id(char)
+        else:
+         
+            char_count[char] = 1
+    
+  
+    return None
+
+
+result = find_first_repeating_character("swiss")
+if result:
+    char, address = result
+    print(f"The first repeating character is '{char}' with memory address {address}.")
+else:
+    print("No repeating character found.")
