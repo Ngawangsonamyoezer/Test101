781762c2709fa04eadc7eb03e468cfe91c16699b
ngawang yoezer <02230066.cst@rub.edu.bt>
2024-05-22 14:48:37 +0600

    reversed linked list

---
+++ reversed linked list.py
@@ -0,0 +1,42 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev_node = None
+        current_node = head
+
+        while current_node is not None:
+            next_node = current_node.next
+            current_node.next = prev_node
+            prev_node = current_node
+            current_node = next_node
+
+        return prev_node
+
+
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.val, end=" -> ")
+        current = current.next
+    print("None")
+
+if __name__ == "__main__":
+    
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    print("Original linked list:")
+    print_linked_list(head)
+
+   
+    sol = Solution()
+    reversed_head = sol.reverseList(head)
+    print("\nReversed linked list:")
+    print_linked_list(reversed_head)
