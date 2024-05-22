fc21f995565eb495e57411c599b02385f4b19650
ngawang yoezer <02230066.cst@rub.edu.bt>
2024-05-22 15:03:24 +0600

    fg

--- logicalactivity (lab 3).py
+++ logicalactivity (lab 3).py
@@ -1,18 +1,7 @@
-
 age = int(input("Enter your age: "))
-<<<<<<< HEAD
-
-
-student = input("Are you a student? (yes/no): ")
-
-discount = (age <= 12) or (13 <= age <= 18 and student == "yes")
-
-=======
 student = input("Are you a student? (yes/no): ")
 discount = (age <= 12) or (13 <= age <= 18 and student == "yes")
->>>>>>> 85b0b010da22e57ac2bb0800bc432d8da9d6c453
 if discount:
     print("You are eligible for a discount.")
 else:
     print("You are not eligible for a discount.")
-
